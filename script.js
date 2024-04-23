f5b5dccaa4ff58148da70a3b34301b03a098fc54
Gourav Singh <gs826399@gmail.com>
2024-04-23 13:47:56 +0530

    ADDING grid

--- script.js
+++ script.js
@@ -3,5 +3,3 @@ function myFunction(x) {
     const A= document.getElementById("navbar");
     A.classList.toggle("visible");
   }
-
-alert("This site is in Development.Please Give your review on our social media handles")
